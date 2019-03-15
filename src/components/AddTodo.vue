83b10ed749b7756a0f78baf724adef7307ce7d71
ticak <tijanak1991@hotmail.com>
2019-03-15 16:29:03 +0100

    todo

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <form @submit.prevent="addTodo">
+            <input type="text" v-model="title" name="title" placeholder="Add Todo...">
+            <input type="submit" value="Submit" class="btn">
+        </form>
+    </div>
+</template>
+
+<script>
+import uuid from 'uuid';
+export default {
+    name:"AddTodo",
+    data(){
+        return {
+            title:" "
+        }
+    },
+    methods:{
+        addTodo(){
+            let newTodo = {
+                id:uuid.v4(),
+                title:this.title,
+                completed:false
+        }
+
+        this.$emit('add-todo',newTodo);
+
+        this.title=''; //da input polje ostane prazno, nakon sto se title doda
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+    input[type="text"]{
+        flex: 10;
+        padding: 5px;
+
+    }
+    input[type="submit"]{
+        float: 2;
+    }
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <AddTodo @add-todo="addTodo" />
+    <Todos v-bind:todos="todos" @del-todo="deleteTodo"/> 
+  </div>
+</template>
+
+<script>
+import Todos from '../components/Todos';
+import AddTodo from '../components/AddTodo'; 
+
+export default {
+  name: 'Home',
+  components: {
+    Todos,
+    AddTodo
+  },
+  data(){
+    return {
+      todos:[
+        {
+          id: 1,
+          title: "Todo One",
+          completed:false
+        },
+        {
+        id: 2,
+        title: "Todo Two",
+        completed:true
+        },
+        {
+        id: 3,
+          title: "Todo Three",
+          completed:false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(id){
+      this.todos = this.todos.filter(todo => todo.id!==id);  
+    },
+    addTodo(newTodo){
+      this.todos=[...this.todos, newTodo];
+    }
+  
+  
+
+  }
+}
+
+</script>
+
+<style>
+*{
+  box-sizing:border-box;
+  margin: 0;
+  padding:0;
+}
+body{
+  font-family:Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding:7px 20px;
+  cursor:pointer;
+}
+input[type="text"]{
+  flex:10;
+  padding:5px;
+}
+</style>
